eb3d6935e617097daffb24184d03e8ed8c1e55ff
shubhangee00 <shubhangeesb@weblineapps.com>
2023-04-03 16:48:37 +0530

    message pass

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
-class Demo{
+class Demo {
     public static void main(String[] args) {
-        int a=20;
-        int b=30;
-        System.out.println(a+b);
+        int a = 20;
+        int b = 30;
+        System.out.println("Addition of two numbers:" + (a + b));
     }
 }
